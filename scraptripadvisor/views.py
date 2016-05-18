d2ece8d704a4a1a9c667417e02f6f3cbedc1a5b1
Bastien <brrav0@yahoo.com>
2016-05-19 04:04:37 +0800

    new try

--- scraptripadvisor/views.py
+++ scraptripadvisor/views.py
@@ -8,7 +8,23 @@ from lxml import html
 import requests
 import re
 
-# Import model here if necessary
+# First index rep about trip advisor
+
+#def index(request):
+#     template = loader.get_template('scraptripadvisor/index.html')
+#     soup = BeautifulSoup(open("scraptripadvisor/Singapore-tripadvisor.html").read())
+#     tList = []
+#     spans = soup.findAll('span', attrs={'class':u'reviewCount'})
+#     for span in spans:
+#       a = span.find('a')
+#       m = re.search(r'\d+',str(a.string))
+#       resto = re.search('(?<=Reviews-)(.*?)(?=-Singapore)',a.get('href')).group(0)
+#       tList.append((str(resto),str(m.group(0))))
+#       tList.sort(key=lambda review: review[1], reverse=True)
+#     context = {'list':tList}
+#     return HttpResponse(template.render(context, request))
+
+# Second option
 
 def index(request):
      template = loader.get_template('scraptripadvisor/index.html')
@@ -21,10 +37,6 @@ def index(request):
        resto = re.search('(?<=Reviews-)(.*?)(?=-Singapore)',a.get('href')).group(0)
        tList.append((str(resto),str(m.group(0))))
        tList.sort(key=lambda review: review[1], reverse=True)
-#     print(soup.prettify())
-#     return HttpResponse("Hello, let's scrap")
-#     return render(request, 'scraptripadvisor/index.html')
      context = {'list':tList}
      return HttpResponse(template.render(context, request))
-# Create your views here.
 
